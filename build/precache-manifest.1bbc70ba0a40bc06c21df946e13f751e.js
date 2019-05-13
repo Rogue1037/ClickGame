2472de0d8ca10e6828c806bd17a8aa8bbeb23bc1
Dylan Davidson <rogue1037@gmail.com>
2019-05-13 13:06:01 -0700

    fixed double folder issue

---
+++ build/precache-manifest.1bbc70ba0a40bc06c21df946e13f751e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9672af854be242be0e684eec356ee99",
+    "url": "/Rogue1037/ClickGame/index.html"
+  },
+  {
+    "revision": "385466546be7f92ea66b",
+    "url": "/Rogue1037/ClickGame/static/css/main.0449f4ea.chunk.css"
+  },
+  {
+    "revision": "ed67e21e0fbb680b5f8f",
+    "url": "/Rogue1037/ClickGame/static/js/2.abafc83a.chunk.js"
+  },
+  {
+    "revision": "385466546be7f92ea66b",
+    "url": "/Rogue1037/ClickGame/static/js/main.9741c140.chunk.js"
+  },
+  {
+    "revision": "60e0482aacb409d9445b",
+    "url": "/Rogue1037/ClickGame/static/js/runtime~main.3d7cf11b.js"
+  },
+  {
+    "revision": "d6f781bdacd7229145e830d49573d9cf",
+    "url": "/Rogue1037/ClickGame/static/media/space.d6f781bd.jpg"
+  }
+]);
